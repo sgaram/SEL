d450a46588f62135de6fea97fc20d3fb6f22aed1
sgaram <43074415+sgaram@users.noreply.github.com>
2018-10-19 00:03:43 +0700

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -30,8 +30,8 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'ADITMADZS'
-    SYSTEM_VER  = '11.2.5'
+    SYSTEM_NAME = 'sc'
+    SYSTEM_VER  = '10.12.0'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
